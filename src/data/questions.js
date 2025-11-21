c6d5f56a272b5eaf5bff60e28866cc66866d20ee
Ardhana <ardhana.gusti@epson.co.id>
2025-11-21 16:23:39 +0700

    Change question

--- src/data/questions.js
+++ src/data/questions.js
@@ -1,7 +1,7 @@
 export const questions = [
   { id: 1, question: 'What is answer from 1st clue?', answer: '22090009' },
-  { id: 2, question: 'When IS Promotion is exist in IEI? (year)', answer: '1' },
-  { id: 3, question: 'How much employee in IS Promotion?', answer: '1' },
-  { id: 4, question: 'How much sub-division of IS Promotion"?', answer: '5' },
-  // { id: 5, question: 'What is the chemical symbol for water?', answer: 'h2o' }
+  { id: 2, question: 'When was PT Indonesia Epson Industry founded? (year)', answer: '1994' },
+  { id: 3, question: 'How much employee in IS Promotion?', answer: '63' },
+  { id: 4, question: 'How much sub-division of IS Promotion?', answer: '5' },
+  { id: 5, question: 'When is ?', answer: 'h2o' }
 ]
